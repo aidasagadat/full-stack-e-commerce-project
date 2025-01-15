b54286ce1eb848a99bf93e6853f7af21c83483d2
aidasagadat <aidasagadat355@gmail.com>
2025-01-15 16:21:45 +0500

    Create Address entity with fields for id, name, locality, address, city, state, bincode, and mobile

---
+++ src/main/java/com/aida/model/Address.java
@@ -0,0 +1,36 @@
+package com.aida.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String locality;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private String bincode;
+
+    private String mobile;
+
+
+}
