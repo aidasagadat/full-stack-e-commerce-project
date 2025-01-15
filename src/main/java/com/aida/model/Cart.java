c6832c2f3a2a2863d7512606187f3b7dcfd9b90b
aidasagadat <aidasagadat355@gmail.com>
2025-01-15 16:22:38 +0500

    Create Cart and CartItem entities with fields for user, cart items, pricing details, and associated relationships

---
+++ src/main/java/com/aida/model/Cart.java
@@ -0,0 +1,39 @@
+package com.aida.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+
+    private Set<CartItem> cartItems = new HashSet<>();
+
+    private double totalSellingPrice;
+
+    private int totalItem;
+
+    private int totalMrpPrice;
+
+    private int discount;
+
+    private String couponCode;
+
+
+
+
+}
