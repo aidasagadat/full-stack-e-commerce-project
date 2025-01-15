9c37ed2657c0bb9e76afabf21b7dfbe5bd9aa1fc
aidasagadat <aidasagadat355@gmail.com>
2025-01-15 16:42:19 +0500

    To product entity add fields for pricing, quantity, color, images, ratings, category, seller, creation date, sizes, and reviews

--- src/main/java/com/aida/model/Product.java
+++ src/main/java/com/aida/model/Product.java
@@ -1,4 +1,77 @@
 package com.aida.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private int mrpPrice;
+
+    private int sellingPrice;
+
+    private int discountPersent;
+
+    private int quantity;
+
+    private String color;
+
+    @ElementCollection
+    private List<String> images = new ArrayList<>();
+
+    private int numRatings;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToOne
+    private Seller seller;
+
+    private LocalDateTime createdAt;
+
+    private String sizes;
+
+    @OneToMany
+    private List<Review> reviews = new ArrayList<>();
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
