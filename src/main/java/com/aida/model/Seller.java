71a0a5a8112c6664402424b75ad05fb0d09b6c87
aidasagadat <aidasagadat355@gmail.com>
2025-01-16 11:22:04 +0500

    add fields to seller class

--- src/main/java/com/aida/model/Seller.java
+++ src/main/java/com/aida/model/Seller.java
@@ -1,7 +1,9 @@
 package com.aida.model;
 
 
-import jakarta.persistence.Entity;
+import com.aida.domain.AccountStatus;
+import com.aida.domain.USER_ROLE;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -11,4 +13,55 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String sellerName;
+
+    private String mobile;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Embedded
+    private BusinessDetails businessDetails = new BusinessDetails();
+
+    @Embedded
+    private BankDetails bankDetails = new BankDetails();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address pickupAddress = new Address();
+
+
+    private String GSTIN;
+
+    private USER_ROLE role = USER_ROLE.ROLE_SELLER;
+
+    private boolean isEmailVerified = false;
+
+    private AccountStatus accountStatus = AccountStatus.PENDING_VERIFICATION;
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
