4009d5bce54b1c903a7c10f6cfb06a9bb87ece09
aidasagadat <aidasagadat355@gmail.com>
2025-01-16 15:53:25 +0500

    create WishList class and add fields

---
+++ src/main/java/com/aida/model/WishList.java
@@ -0,0 +1,39 @@
+package com.aida.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class WishList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @ManyToMany
+    private Set<Product> products = new HashSet<>();
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
