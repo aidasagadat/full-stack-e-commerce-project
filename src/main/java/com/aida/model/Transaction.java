7d5d3a7045e8a23769b09734d4c79c236878aa43
aidasagadat <aidasagadat355@gmail.com>
2025-01-16 16:09:32 +0500

    add Transaction and VerificationCode classes

---
+++ src/main/java/com/aida/model/Transaction.java
@@ -0,0 +1,33 @@
+package com.aida.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User customer;
+
+    @OneToOne
+    private Order order;
+
+    @ManyToOne
+    private Seller seller;
+
+
+    private LocalDateTime date = LocalDateTime.now();
+
+
+}
