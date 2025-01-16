9ede81ea549f8b80e51ea4d58424c250a762b5aa
aidasagadat <aidasagadat355@gmail.com>
2025-01-16 09:50:35 +0500

    Create category, review and seller entities

---
+++ src/main/java/com/aida/model/Category.java
@@ -0,0 +1,52 @@
+package com.aida.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    private String name;
+
+    @NotNull
+    @Column(unique = true)
+    private String categoryId;
+
+    @ManyToOne
+    private Category parentCategory;
+
+    @NotNull
+    private Integer level;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/aida/model/Seller.java
@@ -0,0 +1,14 @@
+package com.aida.model;
+
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Seller {
+}
