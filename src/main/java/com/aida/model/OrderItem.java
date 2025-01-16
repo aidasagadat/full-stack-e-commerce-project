c2364af2cfb7d58235b009c197734422e9ed3906
aidasagadat <aidasagadat355@gmail.com>
2025-01-16 15:29:19 +0500

    create OrderItem class

--- src/main/java/com/aida/model/OrderItem.java
+++ src/main/java/com/aida/model/OrderItem.java
@@ -1,4 +1,20 @@
 package com.aida.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
