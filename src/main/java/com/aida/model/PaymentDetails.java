1a76d5bee5f23bbf850bfef7def10870af21b433
aidasagadat <aidasagadat355@gmail.com>
2025-01-16 15:43:44 +0500

    add fields for PaymentDetails

--- src/main/java/com/aida/model/PaymentDetails.java
+++ src/main/java/com/aida/model/PaymentDetails.java
@@ -1,4 +1,14 @@
 package com.aida.model;
 
+import com.aida.domain.PaymentStatus;
+import lombok.Data;
+
+@Data
 public class PaymentDetails {
+    private String paymentId;
+    private String razorpayPaymentLinkId;
+    private String razorpayPaymentLinkReferenceId;
+    private String razorpayPaymentLinkStatus;
+    private String razorpayPaymentIdZWSP;
+    private PaymentStatus status;
 }
