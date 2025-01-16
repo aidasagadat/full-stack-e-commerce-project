9cf9d0f924cae4cf4e50d5708266073ed5406fbb
aidasagadat <aidasagadat355@gmail.com>
2025-01-16 11:31:47 +0500

    add fields to BankDetails

--- src/main/java/com/aida/model/BankDetails.java
+++ src/main/java/com/aida/model/BankDetails.java
@@ -1,4 +1,10 @@
 package com.aida.model;
 
+import lombok.Data;
+
+@Data
 public class BankDetails {
+    private String accountNumber;
+    private String accountholderName;
+    private String ifscCode;
 }
