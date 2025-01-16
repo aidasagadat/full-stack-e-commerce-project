cb341429e64de06a810c1a46462c04e29dff7d52
aidasagadat <aidasagadat355@gmail.com>
2025-01-16 10:56:37 +0500

    Add fields for review text, rating, product images, product, user, and creation timestamp

--- src/main/java/com/aida/model/Review.java
+++ src/main/java/com/aida/model/Review.java
@@ -1,8 +1,13 @@
 package com.aida.model;
 
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Properties;
 
 @Entity
 @Getter
@@ -11,4 +16,43 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String reviewText;
+
+
+    @Column(nullable = false)
+    private double rating;
+
+    @ElementCollection
+    private List<String> productImages;
+
+    @JsonIgnore
+    @ManyToOne
+    @Column(nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @Column(nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
