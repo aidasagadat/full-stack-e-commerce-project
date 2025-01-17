2ba7038c2938c86abd72bb26553af6a8fd7fb7ad
aidasagadat <aidasagadat355@gmail.com>
2025-01-17 09:52:34 +0500

    add fields to HomeCategory

--- src/main/java/com/aida/model/HomeCategory.java
+++ src/main/java/com/aida/model/HomeCategory.java
@@ -1,7 +1,11 @@
 package com.aida.model;
 
 
+import com.aida.domain.HomeCategorySection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Entity
@@ -11,4 +15,13 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class HomeCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String image;
+    private String categoryId;
+    private HomeCategorySection section;
+
 }
