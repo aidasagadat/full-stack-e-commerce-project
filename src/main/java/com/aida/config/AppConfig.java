881b934e72e7985a01bcd83dd3f2315b3bb418ab
aidasagadat <aidasagadat355@gmail.com>
2025-01-18 13:08:56 +0500

    Add Cors configuration, PasswordEncoder bean, and RestTemplate bean to AppConfig class

--- src/main/java/com/aida/config/AppConfig.java
+++ src/main/java/com/aida/config/AppConfig.java
@@ -1,13 +1,20 @@
 package com.aida.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -31,7 +38,47 @@ public class AppConfig {
 
     private CorsConfigurationSource corsConfigurationSource() {
 
-        return null;
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Collections.singletonList("*"));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setExposedHeaders(Collections.singletonList("Authorization"));
+                cfg.setMaxAge(3600l);
+                return cfg;
+            }
+        };
+
+
+
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
