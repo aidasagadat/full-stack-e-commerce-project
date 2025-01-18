e91464e113ac5a0c06f7ae2d2e1efe8a379d8164
aidasagadat <aidasagadat355@gmail.com>
2025-01-18 10:03:30 +0500

    update AuthController to include request mapping and signup endpoint

--- src/main/java/com/aida/controller/AuthController.java
+++ src/main/java/com/aida/controller/AuthController.java
@@ -5,12 +5,14 @@ import com.aida.response.SignupRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
-    @PostMapping
+    @PostMapping("/signup")
     public ResponseEntity<User> createUserHandler(@RequestBody SignupRequest req){
         User user = new User();
         user.setEmail(req.getEmail());
