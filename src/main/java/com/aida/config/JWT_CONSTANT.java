f59fa551472fc997613c72761d2459e997273b5d
aidasagadat <aidasagadat355@gmail.com>
2025-01-18 13:48:55 +0500

    `Create JWT_CONSTANT class that saves secret key and jw header

---
+++ src/main/java/com/aida/config/JWT_CONSTANT.java
@@ -0,0 +1,9 @@
+package com.aida.config;
+
+import java.util.Stack;
+
+public class JWT_CONSTANT {
+    public static final String SECRET_KEY = "abcdeegthuikwmsnsjweieodkaldaghtwafzqwemioprertyqwertyoir";
+    public static final String JW_HEADER = "Authorization";
+
+}
