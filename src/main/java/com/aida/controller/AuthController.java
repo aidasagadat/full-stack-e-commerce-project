f37ed78513c2e6a2cd1639221726a2a3d29e0f28
aidasagadat <aidasagadat355@gmail.com>
2025-01-18 13:49:06 +0500

    minor refactor

--- src/main/java/com/aida/controller/AuthController.java
+++ src/main/java/com/aida/controller/AuthController.java
@@ -1,16 +1,25 @@
 package com.aida.controller;
 
 import com.aida.model.User;
+import com.aida.repository.UserRepository;
 import com.aida.response.SignupRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AuthController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<User> createUserHandler(@RequestBody SignupRequest req){
@@ -18,20 +27,11 @@ public class AuthController {
         user.setEmail(req.getEmail());
         user.setFullName(req.getFullName());
 
-        return ResponseEntity.ok(user);
+        User savedUser = userRepository.save(user);
+
+        return ResponseEntity.ok(savedUser);
     }
-
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
