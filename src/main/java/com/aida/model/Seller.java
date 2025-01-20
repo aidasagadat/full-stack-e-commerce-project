c3c06eae0fe438bb96869e5fc65b76af81fb99f4
aidasagadat <aidasagadat355@gmail.com>
2025-01-20 22:57:46 +0500

    refactor Seller class to not use lombok

--- src/main/java/com/aida/model/Seller.java
+++ src/main/java/com/aida/model/Seller.java
@@ -1,17 +1,12 @@
 package com.aida.model;
 
+import jakarta.persistence.*;
 
 import com.aida.domain.AccountStatus;
 import com.aida.domain.USER_ROLE;
-import jakarta.persistence.*;
-import lombok.*;
+
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
 public class Seller {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -35,7 +30,6 @@ public class Seller {
     @OneToOne(cascade = CascadeType.ALL)
     private Address pickupAddress = new Address();
 
-
     private String GSTIN;
 
     private USER_ROLE role = USER_ROLE.ROLE_SELLER;
@@ -44,11 +38,141 @@ public class Seller {
 
     private AccountStatus accountStatus = AccountStatus.PENDING_VERIFICATION;
 
+    // No-argument constructor
+    public Seller() {
+    }
 
+    // All-arguments constructor
+    public Seller(Long id, String sellerName, String mobile, String email, String password, BusinessDetails businessDetails,
+                  BankDetails bankDetails, Address pickupAddress, String GSTIN, USER_ROLE role,
+                  boolean isEmailVerified, AccountStatus accountStatus) {
+        this.id = id;
+        this.sellerName = sellerName;
+        this.mobile = mobile;
+        this.email = email;
+        this.password = password;
+        this.businessDetails = businessDetails;
+        this.bankDetails = bankDetails;
+        this.pickupAddress = pickupAddress;
+        this.GSTIN = GSTIN;
+        this.role = role;
+        this.isEmailVerified = isEmailVerified;
+        this.accountStatus = accountStatus;
+    }
 
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BusinessDetails getBusinessDetails() {
+        return businessDetails;
+    }
+
+    public void setBusinessDetails(BusinessDetails businessDetails) {
+        this.businessDetails = businessDetails;
+    }
+
+    public BankDetails getBankDetails() {
+        return bankDetails;
+    }
+
+    public void setBankDetails(BankDetails bankDetails) {
+        this.bankDetails = bankDetails;
+    }
+
+    public Address getPickupAddress() {
+        return pickupAddress;
+    }
+
+    public void setPickupAddress(Address pickupAddress) {
+        this.pickupAddress = pickupAddress;
+    }
+
+    public String getGSTIN() {
+        return GSTIN;
+    }
+
+    public void setGSTIN(String GSTIN) {
+        this.GSTIN = GSTIN;
+    }
+
+    public USER_ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(USER_ROLE role) {
+        this.role = role;
+    }
+
+    public boolean isEmailVerified() {
+        return isEmailVerified;
+    }
+
+    public void setEmailVerified(boolean emailVerified) {
+        isEmailVerified = emailVerified;
+    }
+
+    public AccountStatus getAccountStatus() {
+        return accountStatus;
+    }
+
+    public void setAccountStatus(AccountStatus accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    // Equals and HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Seller seller = (Seller) o;
+
+        return id != null ? id.equals(seller.id) : seller.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
-
 
 
 
