6b8590aceb61b4402a4459edf54dcb6f856ebbb5
aidasagadat <aidasagadat355@gmail.com>
2025-01-20 10:03:52 +0500

    refactor Cart class to remove Lombok dependencies and manually implement constructors, getters, setters, and equals/hashCode methods

--- src/main/java/com/aida/model/Cart.java
+++ src/main/java/com/aida/model/Cart.java
@@ -1,17 +1,11 @@
 package com.aida.model;
 
 import jakarta.persistence.*;
-import lombok.*;
-
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -33,7 +27,99 @@ public class Cart {
 
     private String couponCode;
 
+    // Default constructor
+    public Cart() {
+    }
 
+    // All-args constructor
+    public Cart(Long id, User user, Set<CartItem> cartItems, double totalSellingPrice, int totalItem,
+                int totalMrpPrice, int discount, String couponCode) {
+        this.id = id;
+        this.user = user;
+        this.cartItems = cartItems;
+        this.totalSellingPrice = totalSellingPrice;
+        this.totalItem = totalItem;
+        this.totalMrpPrice = totalMrpPrice;
+        this.discount = discount;
+        this.couponCode = couponCode;
+    }
 
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Set<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(Set<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public double getTotalSellingPrice() {
+        return totalSellingPrice;
+    }
+
+    public void setTotalSellingPrice(double totalSellingPrice) {
+        this.totalSellingPrice = totalSellingPrice;
+    }
+
+    public int getTotalItem() {
+        return totalItem;
+    }
+
+    public void setTotalItem(int totalItem) {
+        this.totalItem = totalItem;
+    }
+
+    public int getTotalMrpPrice() {
+        return totalMrpPrice;
+    }
+
+    public void setTotalMrpPrice(int totalMrpPrice) {
+        this.totalMrpPrice = totalMrpPrice;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public String getCouponCode() {
+        return couponCode;
+    }
+
+    public void setCouponCode(String couponCode) {
+        this.couponCode = couponCode;
+    }
+
+    // Equals and hashCode methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cart cart = (Cart) o;
+        return id != null && id.equals(cart.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
