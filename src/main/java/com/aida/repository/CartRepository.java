8d759e15151ccdb5edd68955be000574f64840b3
aidasagadat <aidasagadat355@gmail.com>
2025-01-20 10:04:22 +0500

    create CartRepository class

---
+++ src/main/java/com/aida/repository/CartRepository.java
@@ -0,0 +1,7 @@
+package com.aida.repository;
+
+import com.aida.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
