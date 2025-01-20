607b07b12b37d646ee91fea1faa259c3e7f067d6
aidasagadat <aidasagadat355@gmail.com>
2025-01-20 22:58:10 +0500

    create SellerRepository with findByEmail method

---
+++ src/main/java/com/aida/repository/SellerRepository.java
@@ -0,0 +1,8 @@
+package com.aida.repository;
+
+import com.aida.model.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+    Seller findByEmail(String email);
+}
