27313ddd1291beef4fe8a0f62557adbe8d0803c3
aidasagadat <aidasagadat355@gmail.com>
2025-01-21 19:22:44 +0500

    Add VerificationCodeRepository for handling verification code data
    - Created VerificationCodeRepository to interact with the VerificationCode entity.
    - Added a method to find VerificationCode by email.
    
    This repository will be used for retrieving and managing verification codes in the system.

---
+++ src/main/java/com/aida/repository/VerificationCodeRepository.java
@@ -0,0 +1,9 @@
+package com.aida.repository;
+
+import com.aida.model.VerificationCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationCodeRepository extends JpaRepository<VerificationCode,
+        Long> {
+    VerificationCode findByEmail(String email);
+}
