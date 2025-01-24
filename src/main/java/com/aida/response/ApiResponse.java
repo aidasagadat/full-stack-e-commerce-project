fa90905d23d81f4ade3c68e83480f44e159505b9
aidasagadat <aidasagadat355@gmail.com>
2025-01-24 09:39:00 +0500

    Create ApiResponse class with message field, getters, setters, and overrides for toString, equals, and hashCode methods

--- src/main/java/com/aida/response/ApiResponse.java
+++ src/main/java/com/aida/response/ApiResponse.java
@@ -1,4 +1,40 @@
 package com.aida.response;
 
+import java.util.Objects;
+
 public class ApiResponse {
+    private String message;
+
+    // Getter
+    public String getMessage() {
+        return message;
+    }
+
+    // Setter
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    // Override toString method
+    @Override
+    public String toString() {
+        return "ApiResponse{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    // Override equals method
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ApiResponse that = (ApiResponse) o;
+        return Objects.equals(message, that.message);
+    }
+
+    // Override hashCode method
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
 }
