de95abebc4c5caabacc854449fd21018c1fc7918
aidasagadat <aidasagadat355@gmail.com>
2025-01-26 22:22:04 +0500

    refactor to not use lombok

--- src/main/java/com/aida/request/LoginOtpRequest.java
+++ src/main/java/com/aida/request/LoginOtpRequest.java
@@ -1,11 +1,34 @@
 package com.aida.request;
 
 import com.aida.domain.USER_ROLE;
-import lombok.Data;
 
-@Data
 public class LoginOtpRequest {
+
     private String email;
     private String password;
     private USER_ROLE role;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public USER_ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(USER_ROLE role) {
+        this.role = role;
+    }
 }
