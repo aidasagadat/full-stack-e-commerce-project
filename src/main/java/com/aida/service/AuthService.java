91ad394fdc29ce73d0d129309f094e294c5d256d
aidasagadat <aidasagadat355@gmail.com>
2025-01-26 21:57:35 +0500

    update AuthService to include role in sentLoginOtp

--- src/main/java/com/aida/service/AuthService.java
+++ src/main/java/com/aida/service/AuthService.java
@@ -1,12 +1,13 @@
 package com.aida.service;
 
+import com.aida.domain.USER_ROLE;
 import com.aida.request.LoginRequest;
 import com.aida.response.AuthResponse;
 import com.aida.response.SignupRequest;
 
 public interface AuthService {
 
-    void sentLoginOtp(String email) throws Exception;
+    void sentLoginOtp(String email, USER_ROLE role) throws Exception;
     String createUser(SignupRequest req) throws Exception;
     AuthResponse signing(LoginRequest req);
 
