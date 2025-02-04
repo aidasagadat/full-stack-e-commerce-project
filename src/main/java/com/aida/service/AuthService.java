a6d7e54584f9ca0200731eb305c83ec680dabad3
aidasagadat <aidasagadat355@gmail.com>
2025-02-04 11:55:51 +0500

    make signing method throw an exception

--- src/main/java/com/aida/service/AuthService.java
+++ src/main/java/com/aida/service/AuthService.java
@@ -9,6 +9,6 @@ public interface AuthService {
 
     void sentLoginOtp(String email, USER_ROLE role) throws Exception;
     String createUser(SignupRequest req) throws Exception;
-    AuthResponse signing(LoginRequest req);
+    AuthResponse signing(LoginRequest req) throws Exception;
 
 }
