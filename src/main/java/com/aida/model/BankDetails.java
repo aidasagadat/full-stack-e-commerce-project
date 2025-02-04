9a05df0e2483c1af1ce9edc3570602c96e9b595a
aidasagadat <aidasagadat355@gmail.com>
2025-02-04 11:56:33 +0500

    refactor to not use lombok

--- src/main/java/com/aida/model/BankDetails.java
+++ src/main/java/com/aida/model/BankDetails.java
@@ -1,10 +1,41 @@
 package com.aida.model;
 
-import lombok.Data;
-
-@Data
 public class BankDetails {
+
     private String accountNumber;
     private String accountHolderName;
     private String ifscCode;
+
+    public BankDetails() {
+    }
+
+    public BankDetails(String accountNumber, String accountHolderName, String ifscCode) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.ifscCode = ifscCode;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public String getIfscCode() {
+        return ifscCode;
+    }
+
+    public void setIfscCode(String ifscCode) {
+        this.ifscCode = ifscCode;
+    }
 }
