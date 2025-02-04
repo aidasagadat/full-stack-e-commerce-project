0a3044173a84d863e06d12f5a86cb51edf3cddbe
aidasagadat <aidasagadat355@gmail.com>
2025-02-04 12:04:32 +0500

    add endpoints

--- src/main/java/com/aida/controller/SellerController.java
+++ src/main/java/com/aida/controller/SellerController.java
@@ -1,18 +1,26 @@
 package com.aida.controller;
 
+import com.aida.config.JwtProvider;
+import com.aida.domain.AccountStatus;
+import com.aida.exception.SellerException;
+import com.aida.model.Seller;
+import com.aida.model.SellerReport;
 import com.aida.model.VerificationCode;
 import com.aida.repository.VerificationCodeRepository;
 import com.aida.request.LoginRequest;
-import com.aida.response.ApiResponse;
 import com.aida.response.AuthResponse;
 import com.aida.service.AuthService;
+import com.aida.service.EmailService;
 import com.aida.service.SellerService;
+import com.aida.util.OtpUtil;
+import jakarta.mail.MessagingException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 @RestController
 @RequestMapping("/sellers")
@@ -21,12 +29,16 @@ public class SellerController {
     private final SellerService sellerService;
     private final VerificationCodeRepository verificationCodeRepository;
     private final AuthService authService;
+    private final EmailService emailService;
+    private final JwtProvider jwtProvider;
 
     @Autowired
-    public SellerController(SellerService sellerService, VerificationCodeRepository verificationCodeRepository, AuthService authService) {
+    public SellerController(SellerService sellerService, VerificationCodeRepository verificationCodeRepository, AuthService authService, EmailService emailService, JwtProvider jwtProvider) {
         this.sellerService = sellerService;
         this.verificationCodeRepository = verificationCodeRepository;
         this.authService = authService;
+        this.emailService = emailService;
+        this.jwtProvider = jwtProvider;
     }
 
     @PostMapping("/login")
@@ -36,8 +48,91 @@ public class SellerController {
         String email = req.getEmail();
 
         req.setEmail("seller_" + email);
+
+        System.out.println(otp +" - "+ email);
         AuthResponse authResponse = authService.signing(req);
 
         return ResponseEntity.ok(authResponse);
     }
+
+    @PatchMapping("/verify/{otp}")
+    public ResponseEntity<Seller> verifySellerEmail(@PathVariable String otp) throws Exception {
+        VerificationCode verificationCode = verificationCodeRepository.findByOtp(otp);
+
+        if(verificationCode==null || !verificationCode.getOtp().equals(otp)){
+            throw new Exception("wrong otp...");
+        }
+
+        Seller seller = sellerService.verifyEmail(verificationCode.getEmail(), otp);
+        return new ResponseEntity<>(seller, HttpStatus.OK);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Seller> createSeller(@RequestBody Seller seller) throws Exception, MessagingException {
+        Seller savedSeller = sellerService.createSeller(seller);
+
+        String otp = OtpUtil.generateOtp();
+
+        VerificationCode verificationCode = new VerificationCode();
+        verificationCode.setOtp(otp);
+        verificationCode.setEmail(seller.getEmail());
+        verificationCodeRepository.save(verificationCode);
+
+        String subject = "SGD Bazaar email verification code";
+        String text = "Welcome to SGD Bazaar, verify your account using this link";
+        String frontend_url = "http://localhost:3000/verify-seller/";
+        emailService.sendVerificationOtpEmail(seller.getEmail(), verificationCode.getOtp(), subject, text+frontend_url);
+
+        return new ResponseEntity<>(savedSeller, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Seller> getSellerById(@PathVariable Long id) throws SellerException {
+        Seller seller = sellerService.getSellerById(id);
+        return new ResponseEntity<>(seller, HttpStatus.OK);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<Seller> getSellerByJwt(@RequestHeader("Authorization") String jwt) throws Exception {
+        Seller seller = sellerService.getSellerProfile(jwt);
+        return new ResponseEntity<>(seller, HttpStatus.OK);
+
+    }
+
+//    @GetMapping("/report")
+//    public ResponseEntity<SellerReport> getSellerrepost(@RequestHeader("Authorization") String jwt) throws Exception {
+//        String email = jwtProvider.getEmailFromJwtToken(jwt);
+//        Seller seller = sellerService.getSellerByEmail(email);
+//        SellerReport report sellerReportService.getSellerRepost(seller);
+//        return new ResponseEntity<>(report, HttpStatus.OK);
+//
+//    }
+//
+
+    @GetMapping
+    public ResponseEntity<List<Seller>> getAllSellers(@RequestParam(required = false)AccountStatus status){
+        List<Seller> sellers = sellerService.getAllSellers(status);
+        return ResponseEntity.ok(sellers);
+    }
+
+    @PatchMapping()
+    public ResponseEntity<Seller> updateSeller(@RequestHeader("Authorization") String jwt, @RequestBody Seller seller) throws Exception {
+        Seller profile = sellerService.getSellerProfile(jwt);
+        Seller updateSeller = sellerService.updateSeller(profile.getId(), seller);
+        return ResponseEntity.ok(updateSeller);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSeller(@PathVariable Long id) throws Exception {
+        sellerService.deleteSeller(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
+
 }
