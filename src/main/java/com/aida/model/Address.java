51be68ae62e21db5b8bb794613bd2e735c727b43
aidasagadat <aidasagadat355@gmail.com>
2025-02-04 11:54:44 +0500

    refactor to not use lombok

--- src/main/java/com/aida/model/Address.java
+++ src/main/java/com/aida/model/Address.java
@@ -4,14 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
 public class Address {
 
     @Id
@@ -19,18 +13,105 @@ public class Address {
     private Long id;
 
     private String name;
-
     private String locality;
-
     private String address;
-
     private String city;
-
     private String state;
-
     private String pinCode;
-
     private String mobile;
 
+    // Default constructor
+    public Address() {
+    }
 
+    // Constructor with all fields
+    public Address(Long id, String name, String locality, String address, String city, String state, String pinCode, String mobile) {
+        this.id = id;
+        this.name = name;
+        this.locality = locality;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.pinCode = pinCode;
+        this.mobile = mobile;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    // Override equals and hashCode (based on the id field)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address1 = (Address) o;
+        return id != null && id.equals(address1.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
