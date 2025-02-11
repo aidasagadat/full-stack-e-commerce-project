36de3d71246392f3e7f6187b025d0bd9db88fc3b
aidasagadat <aidasagadat355@gmail.com>
2025-02-11 22:42:31 +0500

    inject CartItemRepository dependency

--- src/main/java/com/aida/service/impl/CartItemServiceImpl.java
+++ src/main/java/com/aida/service/impl/CartItemServiceImpl.java
@@ -1,11 +1,21 @@
 package com.aida.service.impl;
 
 import com.aida.model.CartItem;
+import com.aida.repository.CartItemRepository;
 import com.aida.service.CartItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CartItemServiceImpl implements CartItemService {
+
+    private final CartItemRepository cartItemRepository;
+
+    public CartItemServiceImpl(CartItemRepository cartItemRepository) {
+        this.cartItemRepository = cartItemRepository;
+    }
+
+
     @Override
     public CartItem updateCartItem(Long userId, Long id, CartItem cartItem) {
         return null;
