8b4c6908e0829a791c2ecc822916163f1b765556
aidasagadat <aidasagadat355@gmail.com>
2025-02-11 10:12:34 +0500

    add method for adding cart item

--- src/main/java/com/aida/service/CartService.java
+++ src/main/java/com/aida/service/CartService.java
@@ -1,4 +1,14 @@
 package com.aida.service;
 
+import com.aida.model.CartItem;
+import com.aida.model.Product;
+import com.aida.model.User;
+
 public interface CartService {
+    public CartItem addCartItem(
+            User user,
+            Product product,
+            String size,
+            int quantity
+    );
 }
