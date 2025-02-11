328ea705145b4a370160a0f75bebd601f580820b
aidasagadat <aidasagadat355@gmail.com>
2025-02-11 22:38:32 +0500

    add method to find cartitem by id

--- src/main/java/com/aida/service/CartItemService.java
+++ src/main/java/com/aida/service/CartItemService.java
@@ -5,4 +5,5 @@ import com.aida.model.CartItem;
 public interface CartItemService {
     CartItem updateCartItem(Long userId, Long id, CartItem cartItem);
     void removeCartItem(Long userId, Long cartItemId);
+    CartItem findCartItemById(Long id);
 }
