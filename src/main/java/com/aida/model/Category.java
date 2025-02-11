af01cc8398bdc786e68de768adac047e26c25184
aidasagadat <aidasagadat355@gmail.com>
2025-02-11 10:02:22 +0500

    refactor to not use lombok

--- src/main/java/com/aida/model/Category.java
+++ src/main/java/com/aida/model/Category.java
@@ -1,22 +1,14 @@
 package com.aida.model;
 
-
 import jakarta.persistence.*;
-import lombok.*;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
 
     private String name;
 
@@ -30,7 +22,79 @@ public class Category {
     @NotNull
     private Integer level;
 
+    public Category() {}
 
+    public Category(Long id, String name, String categoryId, Category parentCategory, Integer level) {
+        this.id = id;
+        this.name = name;
+        this.categoryId = categoryId;
+        this.parentCategory = parentCategory;
+        this.level = level;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Category getParentCategory() {
+        return parentCategory;
+    }
+
+    public void setParentCategory(Category parentCategory) {
+        this.parentCategory = parentCategory;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id != null && id.equals(category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", categoryId='" + categoryId + '\'' +
+                ", parentCategory=" + (parentCategory != null ? parentCategory.getId() : "null") +
+                ", level=" + level +
+                '}';
+    }
 
 }
 
