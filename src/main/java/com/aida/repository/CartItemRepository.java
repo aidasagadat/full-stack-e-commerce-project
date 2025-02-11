92d39e9b70ac859b9f2d4af09d098c875227ec95
aidasagadat <aidasagadat355@gmail.com>
2025-02-11 13:35:18 +0500

    replace findByUserId method to method that finds by cart, product and size

--- src/main/java/com/aida/repository/CartItemRepository.java
+++ src/main/java/com/aida/repository/CartItemRepository.java
@@ -2,8 +2,11 @@ package com.aida.repository;
 
 import com.aida.model.Cart;
 import com.aida.model.CartItem;
+import com.aida.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartItemRepository extends JpaRepository<Cart, Long> {
-    Cart findByUserId(Long id) ;
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    CartItem findByCartAndProductAndSize(Cart cart, Product product, String size);
+
 }
