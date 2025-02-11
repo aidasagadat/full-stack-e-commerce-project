90b2c19c4f3f6c033954535fb8fbcfffbabfef9b
aidasagadat <aidasagadat355@gmail.com>
2025-02-11 13:36:19 +0500

    add logic for addCartItem and findUserCart methods

--- src/main/java/com/aida/service/impl/CartServiceImpl.java
+++ src/main/java/com/aida/service/impl/CartServiceImpl.java
@@ -22,11 +22,58 @@ public class CartServiceImpl implements CartService {
 
     @Override
     public CartItem addCartItem(User user, Product product, String size, int quantity) {
+        Cart cart = findUserCart(user);
+        CartItem  isPresent = cartItemRepository.findByCartAndProductAndSize(cart, product, size);
+
+        if(isPresent == null){
+            CartItem cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(quantity);
+            cartItem.setUserId(user.getId());
+            cartItem.setSize(size);
+            int totalPrice = quantity* product.getSellingPrice();
+            cartItem.setSellingPrice(totalPrice);
+            cart.getCartItems().add(cartItem);
+            cartItem.setCart(cart);
+
+            return cartItemRepository.save(cartItem);
+        }
+
         return null;
     }
 
     @Override
     public Cart findUserCart(User user) {
-        return null;
+        Cart cart = cartRepository.findByUserId(user.getId());
+
+        int totalPrice = 0;
+        int totalDiscountedPrice = 0;
+        int totalItem = 0;
+
+        for(CartItem cartItem: cart.getCartItems()){
+             totalPrice += cartItem.getMrpPrice();
+             totalDiscountedPrice += cartItem.getSellingPrice();
+             totalItem+=cartItem.getQuantity();
+        }
+
+        cart.setTotalMrpPrice(totalPrice);
+        cart.setTotalItem(totalItem);
+        cart.setTotalSellingPrice(totalDiscountedPrice);
+        cart.setDiscount(calculateDiscountPercentage(totalPrice, totalDiscountedPrice));
+
+
+        return cart;
     }
+
+    private int calculateDiscountPercentage(int mrpPrice, int sellingPrice) {
+        if(mrpPrice <= 0){
+            throw new IllegalArgumentException("Actual price must be greater than 0");
+        }
+        double discount = mrpPrice-sellingPrice;
+        double discountPercentage =(discount/mrpPrice)*100;
+        System.out.println(discountPercentage);
+        return (int)discountPercentage;
+    }
+
+
 }
