f3203ecf792b32170989c3934e2d6b5324bb52aa
aidasagadat <aidasagadat355@gmail.com>
2025-02-11 10:20:19 +0500

    add method for finding cart by user id

--- src/main/java/com/aida/repository/CartItemRepository.java
+++ src/main/java/com/aida/repository/CartItemRepository.java
@@ -5,5 +5,5 @@ import com.aida.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<Cart, Long> {
-    Cart findByUserId(Long id);
+    Cart findByUserId(Long id) ;
 }
