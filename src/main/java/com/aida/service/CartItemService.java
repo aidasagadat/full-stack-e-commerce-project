688769e35944067eb1fe0a3db8fcc5d1bcc6e642
aidasagadat <aidasagadat355@gmail.com>
2025-02-11 22:37:11 +0500

    create CartItemService with 2 methods: updateCartItem and removeCartItem

---
+++ src/main/java/com/aida/service/CartItemService.java
@@ -0,0 +1,8 @@
+package com.aida.service;
+
+import com.aida.model.CartItem;
+
+public interface CartItemService {
+    CartItem updateCartItem(Long userId, Long id, CartItem cartItem);
+    void removeCartItem(Long userId, Long cartItemId);
+}
