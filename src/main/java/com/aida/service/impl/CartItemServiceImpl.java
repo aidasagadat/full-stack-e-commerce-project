43042209351897b79bf626ae23fba47560318ab9
aidasagadat <aidasagadat355@gmail.com>
2025-02-13 09:28:32 +0500

    add logic to updateCartItem, removeCartItem and findCartItemById methods

--- src/main/java/com/aida/service/impl/CartItemServiceImpl.java
+++ src/main/java/com/aida/service/impl/CartItemServiceImpl.java
@@ -1,6 +1,7 @@
 package com.aida.service.impl;
 
 import com.aida.model.CartItem;
+import com.aida.model.User;
 import com.aida.repository.CartItemRepository;
 import com.aida.service.CartItemService;
 import lombok.RequiredArgsConstructor;
@@ -15,19 +16,42 @@ public class CartItemServiceImpl implements CartItemService {
         this.cartItemRepository = cartItemRepository;
     }
 
-
     @Override
-    public CartItem updateCartItem(Long userId, Long id, CartItem cartItem) {
-        return null;
+    public CartItem updateCartItem(Long userId, Long id, CartItem cartItem) throws Exception {
+
+        CartItem item = findCartItemById(id);
+        User cartItemUser = item.getCart().getUser();
+
+        if (cartItemUser.getId().equals(userId)) {
+
+            item.setQuantity(cartItem.getQuantity());
+            item.setMrpPrice((item.getQuantity()*item.getProduct().getMrpPrice()));
+            item.setSellingPrice(item.getQuantity()*item.getProduct().getSellingPrice());
+            return cartItemRepository.save(item);
+        }
+
+        throw new Exception("you cant update this cart item");
     }
 
     @Override
-    public void removeCartItem(Long userId, Long cartItemId) {
+    public void removeCartItem(Long userId, Long cartItemId) throws Exception {
+
+        CartItem item = findCartItemById((cartItemId));
+
+        User cartItemUser = item.getCart().getUser();
+
+        if(cartItemUser.getId().equals(userId)){
+            cartItemRepository.delete(item);
+        }
+
+        else throw new Exception("you can't delete this item");
+
 
     }
 
     @Override
-    public CartItem findCartItemById(Long id) {
-        return null;
+    public CartItem findCartItemById(Long id) throws Exception {
+        return cartItemRepository.findById(id).orElseThrow(() ->
+                new Exception("cart item not found with id " + id));
     }
 }
