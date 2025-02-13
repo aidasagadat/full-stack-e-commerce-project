e199104a3465137ffd9ef172413e0c0deaa81e64
aidasagadat <aidasagadat355@gmail.com>
2025-02-13 09:33:12 +0500

    add put mapping with an endpoind to add item to cart

--- src/main/java/com/aida/controller/CartController.java
+++ src/main/java/com/aida/controller/CartController.java
@@ -1,16 +1,15 @@
 package com.aida.controller;
 
 import com.aida.model.Cart;
+import com.aida.model.CartItem;
 import com.aida.model.User;
+import com.aida.request.AddItemRequest;
 import com.aida.service.CartItemService;
 import com.aida.service.CartService;
 import com.aida.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -35,6 +34,11 @@ public class CartController {
         System.out.println("cart - " + cart.getUser().getEmail());
 
         return new ResponseEntity<Cart>(cart, HttpStatus.OK);
+
+    }
+
+    @PutMapping("/add")
+    public ResponseEntity<CartItem> addItemToCart(@RequestBody AddItemRequest req, @RequestHeader("Authorization") String jwt){
 
     }
 
