63c49a9262459457a729e0ab42a3185a8c5d1698
aidasagadat <aidasagadat355@gmail.com>
2025-02-13 09:50:14 +0500

    write logic for addItemToCart method

--- src/main/java/com/aida/controller/CartController.java
+++ src/main/java/com/aida/controller/CartController.java
@@ -2,10 +2,13 @@ package com.aida.controller;
 
 import com.aida.model.Cart;
 import com.aida.model.CartItem;
+import com.aida.model.Product;
 import com.aida.model.User;
 import com.aida.request.AddItemRequest;
+import com.aida.response.ApiResponse;
 import com.aida.service.CartItemService;
 import com.aida.service.CartService;
+import com.aida.service.ProductService;
 import com.aida.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +21,13 @@ public class CartController {
     private final CartService cartService;
     private final CartItemService cartItemService;
     private final UserService userService;
+    private final ProductService productService;
 
-    public CartController(CartService cartService, CartItemService cartItemService, UserService userService) {
+    public CartController(CartService cartService, CartItemService cartItemService, UserService userService, ProductService productService) {
         this.cartService = cartService;
         this.cartItemService = cartItemService;
         this.userService = userService;
+        this.productService = productService;
     }
 
     @GetMapping
@@ -38,7 +43,17 @@ public class CartController {
     }
 
     @PutMapping("/add")
-    public ResponseEntity<CartItem> addItemToCart(@RequestBody AddItemRequest req, @RequestHeader("Authorization") String jwt){
+    public ResponseEntity<CartItem> addItemToCart(@RequestBody AddItemRequest req, @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+        Product product = productService.findProductById(req.getProductId());
+
+        CartItem item = cartService.addCartItem(user, product, req.getSize(), req.getQuantity());
+
+        ApiResponse res = new ApiResponse();
+        res.setMessage("Item added to cart");
+
+        return new ResponseEntity<>(item, HttpStatus.ACCEPTED);
 
     }
 
