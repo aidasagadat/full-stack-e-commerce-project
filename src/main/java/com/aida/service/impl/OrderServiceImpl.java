87c34ab6b90f7daf9ff2628d1d5d7a01f37ee640
aidasagadat <aidasagadat355@gmail.com>
2025-02-25 09:44:56 +0500

    create OrderServiceImpl class to implement OrderService interface

---
+++ src/main/java/com/aida/service/impl/OrderServiceImpl.java
@@ -0,0 +1,43 @@
+package com.aida.service.impl;
+
+import com.aida.domain.OrderStatus;
+import com.aida.model.Address;
+import com.aida.model.Cart;
+import com.aida.model.Order;
+import com.aida.model.User;
+import com.aida.service.OrderService;
+
+import java.util.List;
+import java.util.Set;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public Set<Order> createOrder(User user, Address shippingAddress, Cart cart) {
+        return Set.of();
+    }
+
+    @Override
+    public Order findOrderById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Order> userOrderHistory(Long userId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Order> sellersOrder(Long sellerId) {
+        return List.of();
+    }
+
+    @Override
+    public Order updateOrderStatus(Long orderId, OrderStatus orderStatus) {
+        return null;
+    }
+
+    @Override
+    public Order cancelOrder(Long orderId, User user) {
+        return null;
+    }
+}
