a415f73a5536cb4636065f640666746b6e2d1840
aidasagadat <aidasagadat355@gmail.com>
2025-02-25 10:19:57 +0500

    add logic for createOrder method

--- src/main/java/com/aida/service/impl/OrderServiceImpl.java
+++ src/main/java/com/aida/service/impl/OrderServiceImpl.java
@@ -25,6 +25,55 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Set<Order> createOrder(User user, Address shippingAddress, Cart cart) {
+        if(!user.getAddresses().contains(shippingAddress)){
+            user.getAddresses().add(shippingAddress);
+        }
+        Address address = addressRepository.save(shippingAddress);
+
+        Map<Long, List<CartItem>> itemsBySeller = cart.getCartItems().stream()
+                .collect(Collectors.groupingBy(item -> item.getProduct()
+                        .getSeller().getId()));
+        Set<Order> orders = new HashSet<>();
+
+        for (Map.Entry<Long, List<CartItem>> entry:itemsBySeller.entrySet()){
+            Long sellerId = entry.getKey();
+            List<CartItem> items = entry.getValue();
+
+            int totalOrderPrice = items.stream().mapToInt(CartItem::getSellingPrice).sum();
+            int totalItem = items.stream().mapToInt(CartItem::getQuantity).sum();
+            Order createdOrder = new Order();
+            createdOrder.setUser(user);
+            createdOrder.setSellerId(sellerId);
+            createdOrder.setTotalMrpPrice(totalOrderPrice);
+            createdOrder.setTotalSellingPrice(totalOrderPrice);
+            createdOrder.setTotalItem(totalItem);
+            createdOrder.setShippingAddress(address);
+            createdOrder.setOrderStatus(OrderStatus.PENDING);
+            createdOrder.getPaymentDetails().setStatus(PaymentStatus.PENDING);
+
+            Order savedOrder = orderRepository.save(createdOrder);
+            orders.add(savedOrder);
+
+            List<OrderItem> orderItems = new ArrayList<>();
+
+            for(CartItem item:items){
+                OrderItem orderItem = new OrderItem();
+                orderItem.setOrder(savedOrder);
+                orderItem.setMrpPrice(item.getMrpPrice());
+                orderItem.setProduct(item.getProduct());
+                orderItem.setQuantity(item.getQuantity());
+                orderItem.setSize(item.getSize());
+                orderItem.setUserId(item.getUserId());
+                orderItem.setSellingPrice(item.getSellingPrice());
+
+                savedOrder.getOrderItems().add(orderItem);
+
+                OrderItem savedOrderItem = orderItemRepository.save(orderItem);
+                orderItems.add(savedOrderItem);
+            }
+            return orders;
+        }
+
         return Set.of();
     }
 
