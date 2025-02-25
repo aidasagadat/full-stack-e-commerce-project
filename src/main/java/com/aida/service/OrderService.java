b5917d80b858726a673cffb0eb73bab038e12d8a
aidasagadat <aidasagadat355@gmail.com>
2025-02-25 09:44:33 +0500

    create OrderService interface with methods

---
+++ src/main/java/com/aida/service/OrderService.java
@@ -0,0 +1,19 @@
+package com.aida.service;
+
+import com.aida.domain.OrderStatus;
+import com.aida.model.Address;
+import com.aida.model.Cart;
+import com.aida.model.Order;
+import com.aida.model.User;
+
+import java.util.List;
+import java.util.Set;
+
+public interface OrderService {
+    Set<Order> createOrder(User user, Address shippingAddress, Cart cart);
+    Order findOrderById(long id);
+    List<Order> userOrderHistory(Long userId);
+    List<Order> sellersOrder(Long sellerId);
+    Order updateOrderStatus(Long orderId, OrderStatus orderStatus);
+    Order cancelOrder(Long orderId, User user);
+}
