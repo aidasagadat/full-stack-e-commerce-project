558e90fe8fe7d7e511c05bb9d96aed9339626e74
aidasagadat <aidasagadat355@gmail.com>
2025-02-25 10:36:31 +0500

    make findOrderById method throw exception

--- src/main/java/com/aida/service/OrderService.java
+++ src/main/java/com/aida/service/OrderService.java
@@ -11,7 +11,7 @@ import java.util.Set;
 
 public interface OrderService {
     Set<Order> createOrder(User user, Address shippingAddress, Cart cart);
-    Order findOrderById(long id);
+    Order findOrderById(long id) throws Exception;
     List<Order> userOrderHistory(Long userId);
     List<Order> sellersOrder(Long sellerId);
     Order updateOrderStatus(Long orderId, OrderStatus orderStatus);
