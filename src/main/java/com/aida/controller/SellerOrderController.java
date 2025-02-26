7fc44a077685b27705d87a4794b943d5d0a0fd0e
aidasagadat <aidasagadat355@gmail.com>
2025-02-26 09:08:00 +0500

    add logic to SellerOrderController

--- src/main/java/com/aida/controller/SellerOrderController.java
+++ src/main/java/com/aida/controller/SellerOrderController.java
@@ -1,11 +1,34 @@
 package com.aida.controller;
 
+import com.aida.domain.OrderStatus;
+import com.aida.model.Order;
+import com.aida.model.Seller;
+import com.aida.service.OrderService;
+import com.aida.service.SellerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/seller/orders")
+@RequestMapping("/api/seller/orders")
 public class SellerOrderController {
+    private final OrderService orderService;
+    private final SellerService sellerService;
+
+    @GetMapping()
+    public ResponseEntity<List<Order>> getAllOrdersHandler(@RequestHeader("Authorization") String jwt) throws Exception {
+        Seller seller = sellerService.getSellerProfile(jwt);
+        List<Order> orders = orderService.sellersOrder(seller.getId());
+        return new ResponseEntity<>(orders, HttpStatus.ACCEPTED);
+    }
+
+    @PatchMapping("/{orderId}/status/{orderStatus}")
+    public ResponseEntity<Order> updateOrderHandler(@RequestHeader("Authorization") String jwt, @PathVariable Long orderId, @PathVariable OrderStatus orderStatus) throws Exception {
+        Order order = orderService.updateOrderStatus(orderId, orderStatus);
+        return new ResponseEntity<>(order, HttpStatus.ACCEPTED);
+    }
 }
