0978d9efaffce0cc87a4d72cff6603fd4de63181
aidasagadat <aidasagadat355@gmail.com>
2025-02-26 08:49:00 +0500

    add new endpoints and logic to OrderController

--- src/main/java/com/aida/controller/OrderController.java
+++ src/main/java/com/aida/controller/OrderController.java
@@ -1,10 +1,18 @@
 package com.aida.controller;
 
+import com.aida.domain.PaymentMethod;
+import com.aida.model.*;
+import com.aida.response.PaymentLinkResponse;
+import com.aida.service.CartService;
 import com.aida.service.OrderService;
 import com.aida.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,6 +21,53 @@ public class OrderController {
 
     private final OrderService orderService;
     private final UserService userService;
+    private final CartService cartService;
+
+    @PostMapping()
+    public ResponseEntity<PaymentLinkResponse> createOrderHandler(
+            @RequestHeader("Authorization") String jwt,
+            @RequestBody Address shippingAddress,
+            @RequestParam PaymentMethod paymentMethod
+    ) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+        Cart cart = cartService.findUserCart(user);
+        Set<Order> orders = orderService.createOrder(user, shippingAddress, cart);
+
+        PaymentLinkResponse res = new PaymentLinkResponse();
+
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<Order>> usersOrderHistoryHandler(@RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        List<Order> orders = orderService.userOrderHistory(user.getId());
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Long orderId, @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        Order order = orderService.findOrderById(orderId);
+        return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+    @GetMapping("/item/{orderItemId}")
+    public ResponseEntity<OrderItem> getOrderItemById(@PathVariable Long orderItemId, @RequestHeader("Authorization") String jwt) throws Exception {
+        System.out.println("----------controller");
+        User user = userService.findUserByJwtToken(jwt);
+        OrderItem orderItem = orderService.getOrderItemById(orderItemId);
+        return new ResponseEntity<>(orderItem, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/{orderId}/cancel")
+    public ResponseEntity<Order> cancelOrder(@PathVariable Long orderId, @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        Order order = orderService.cancelOrder(orderId, user);
+
+        return ResponseEntity.ok(order);
+    }
 
 
 }
