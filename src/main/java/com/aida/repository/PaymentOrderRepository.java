e7125e8ce5be90eb249a977c812177bc3d9ba663
aidasagadat <aidasagadat355@gmail.com>
2025-02-27 10:46:14 +0500

    create PaymentOrderRepository and add findByPaymentLinkId method

---
+++ src/main/java/com/aida/repository/PaymentOrderRepository.java
@@ -0,0 +1,8 @@
+package com.aida.repository;
+
+import com.aida.model.PaymentOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentOrderRepository extends JpaRepository<PaymentOrder, Long> {
+    PaymentOrder findByPaymentLinkId(String paymentId);
+}
