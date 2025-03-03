be99fca4885babdb19efb942a48392fcd13e7e34
aidasagadat <aidasagadat355@gmail.com>
2025-03-03 09:08:25 +0500

    add methods for razorpay

--- src/main/java/com/aida/service/PaymentService.java
+++ src/main/java/com/aida/service/PaymentService.java
@@ -3,8 +3,9 @@ package com.aida.service;
 import com.aida.model.Order;
 import com.aida.model.PaymentOrder;
 import com.aida.model.User;
+import com.razorpay.PaymentLink;
+import com.razorpay.RazorpayException;
 import com.stripe.exception.StripeException;
-import com.stripe.model.PaymentLink;
 
 import java.util.Set;
 
@@ -12,9 +13,9 @@ public interface PaymentService {
     PaymentOrder createOrder(User user, Set<Order> orders);
     PaymentOrder getPaymentOrderById(Long orderId) throws Exception;
     PaymentOrder getPaymentOrderByPaymentId(String orderId) throws Exception;
-    Boolean ProceedPaymentOrder(PaymentOrder paymentOrder, String paymentId, String paymentLinkId);
+    Boolean ProceedPaymentOrder(PaymentOrder paymentOrder, String paymentId, String paymentLinkId) throws RazorpayException;
+    PaymentLink createRazorpayPaymentLink(User user, Long amount, Long orderId);
     String createStripePaymentLink(User user, Long amount, Long orderId) throws StripeException;
-
 }
 
 
