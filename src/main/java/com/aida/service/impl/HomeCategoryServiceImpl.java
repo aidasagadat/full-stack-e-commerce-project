c6b08c797b33a40c5ef059e288e08a1e7c82a66d
aidasagadat <aidasagadat355@gmail.com>
2025-03-06 12:09:17 +0500

    implement methods and perform injection

--- src/main/java/com/aida/service/impl/HomeCategoryServiceImpl.java
+++ src/main/java/com/aida/service/impl/HomeCategoryServiceImpl.java
@@ -1,10 +1,35 @@
 package com.aida.service.impl;
 
+import com.aida.model.HomeCategory;
+import com.aida.repository.HomeCategoryRepository;
 import com.aida.service.HomeCategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class HomeCategoryServiceImpl implements HomeCategoryService {
+    private HomeCategoryRepository homeCategoryRepository;
+
+    @Override
+    public HomeCategory createHomeCategory(HomeCategory homeCategory) {
+        return null;
+    }
+
+    @Override
+    public List<HomeCategory> createCategories(List<HomeCategory> homeCategories) {
+        return List.of();
+    }
+
+    @Override
+    public HomeCategory updateHomeCategory(HomeCategory homeCategory, Long id) {
+        return null;
+    }
+
+    @Override
+    public List<HomeCategory> getALlHomeCategories() {
+        return List.of();
+    }
 }
