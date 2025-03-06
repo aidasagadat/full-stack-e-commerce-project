b0f76a706f6cde4568de58a04c280569fba5d9cf
aidasagadat <aidasagadat355@gmail.com>
2025-03-06 12:08:33 +0500

    add necessary annotations

--- src/main/java/com/aida/service/impl/HomeCategoryServiceImpl.java
+++ src/main/java/com/aida/service/impl/HomeCategoryServiceImpl.java
@@ -1,6 +1,10 @@
 package com.aida.service.impl;
 
 import com.aida.service.HomeCategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class HomeCategoryServiceImpl implements HomeCategoryService {
 }
