8c9d88c168e8c1002b16e88618f8b09db83e0efc
aidasagadat <aidasagadat355@gmail.com>
2025-03-06 11:27:13 +0500

    perform injections

--- src/main/java/com/aida/controller/AdminCouponController.java
+++ src/main/java/com/aida/controller/AdminCouponController.java
@@ -1,5 +1,8 @@
 package com.aida.controller;
 
+import com.aida.service.CartService;
+import com.aida.service.CouponService;
+import com.aida.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping
 public class AdminCouponController {
+    private final CouponService couponService;
+    private final UserService userService;
+    private final CartService cartService;
 }
