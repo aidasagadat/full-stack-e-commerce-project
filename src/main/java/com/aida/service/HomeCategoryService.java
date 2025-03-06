991e2ed4637d294c4504913c5dbe87a2a4dc07da
aidasagadat <aidasagadat355@gmail.com>
2025-03-06 12:07:40 +0500

    create HomeCategoryService with four methods

---
+++ src/main/java/com/aida/service/HomeCategoryService.java
@@ -0,0 +1,13 @@
+package com.aida.service;
+
+import com.aida.model.Home;
+import com.aida.model.HomeCategory;
+
+import java.util.List;
+
+public interface HomeCategoryService {
+    HomeCategory createHomeCategory(HomeCategory homeCategory);
+    List<HomeCategory> createCategories(List<HomeCategory> homeCategories);
+    HomeCategory updateHomeCategory(HomeCategory homeCategory, Long id);
+    List<HomeCategory> getALlHomeCategories();
+}
