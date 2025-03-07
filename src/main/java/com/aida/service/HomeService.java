3f15782fa5a6a8aa093cec9e507ab7e91efb94a1
aidasagadat <aidasagadat355@gmail.com>
2025-03-07 11:43:51 +0500

    add createHomePageData method

--- src/main/java/com/aida/service/HomeService.java
+++ src/main/java/com/aida/service/HomeService.java
@@ -1,4 +1,10 @@
 package com.aida.service;
 
+import com.aida.model.Home;
+import com.aida.model.HomeCategory;
+
+import java.util.List;
+
 public interface HomeService {
+    public Home createHomePageData(List<HomeCategory> allCategories);
 }
